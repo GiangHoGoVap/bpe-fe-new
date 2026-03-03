d05b31beae2328e46195ced458c46ff19af5a5e3
Nguyễn Minh Tâm <minhtam27022001@gmail.com>
2026-03-03 17:31:45 +0700

    Fix UI of creating workspace box

--- src/components/CreateWorkspaceButton/CreateWorkspaceButton.tsx
+++ src/components/CreateWorkspaceButton/CreateWorkspaceButton.tsx
@@ -2,138 +2,155 @@ import workspaceApi from "@/api/workspace";
 import useNotification from "@/hooks/useNotification";
 import { IWorkspace } from "@/interfaces/workspaces";
 import {
-  Button,
-  Divider,
-  Group,
-  Input,
-  Modal,
-  TextInput,
-  Textarea,
+    Button,
+    Divider,
+    Group,
+    Input,
+    Modal,
+    TextInput,
+    Textarea,
 } from "@mantine/core";
 import { ReactComponent as IconPlus } from "@tabler/icons/icons/plus.svg";
 import { useState } from "react";
 import { useCreateWorkspaceButtonStyle } from "./CreateWorkspaceButton.style";
 
 const CreateWorkspaceButton = ({
-  onCreateWorkspace,
+    onCreateWorkspace,
 }: {
-  onCreateWorkspace: (workspace: IWorkspace) => void;
+    onCreateWorkspace: (workspace: IWorkspace) => void;
 }) => {
-  const [open, setOpen] = useState(false);
-  const [workspaceName, setWorkspaceName] = useState<string>();
-  const [workspaceDescription, setWorkspaceDescription] = useState<string>();
-  const notify = useNotification();
-  const { classes } = useCreateWorkspaceButtonStyle();
+    const [open, setOpen] = useState(false);
+    const [workspaceName, setWorkspaceName] = useState<string>();
+    const [workspaceDescription, setWorkspaceDescription] = useState<string>();
+    const notify = useNotification();
+    const { classes } = useCreateWorkspaceButtonStyle();
 
-  const onClear = () => {
-    setOpen(false);
-    setWorkspaceName(undefined);
-    setWorkspaceDescription(undefined);
-  };
+    const onClear = () => {
+        setOpen(false);
+        setWorkspaceName(undefined);
+        setWorkspaceDescription(undefined);
+    };
 
-  const onCreateNewWorkspace = async () => {
-    try {
-      if (workspaceName?.includes("/") || workspaceName?.includes("?")) {
-        notify({
-          title: "Error!",
-          message: "Special characters are not allowed in workspace's name!",
-          type: "error",
-        });
-        return;
-      }
-      if (!workspaceName || !workspaceDescription) {
-        notify({
-          title: "Error!",
-          message: `${
-            !workspaceName && !workspaceDescription
-              ? `Workspace's name and description`
-              : !workspaceDescription
-              ? `Workspace's description`
-              : `Workspace's name`
-          } can not be empty!`,
-          type: "error",
-        });
-      }
-      if (workspaceName && workspaceDescription) {
-        const res = await workspaceApi.createWorkspace({
-          name: workspaceName,
-          description: workspaceDescription,
-        });
-        if (res) {
-          onClear();
-          notify({
-            title: "Success!",
-            message: "You have successfully created a new workspace!",
-            type: "success",
-          });
-          onCreateWorkspace(res);
+    const onCreateNewWorkspace = async () => {
+        try {
+            if (workspaceName?.includes("/") || workspaceName?.includes("?")) {
+                notify({
+                    title: "Error!",
+                    message:
+                        "Special characters are not allowed in workspace's name!",
+                    type: "error",
+                });
+                return;
+            }
+            if (!workspaceName || !workspaceDescription) {
+                notify({
+                    title: "Error!",
+                    message: `${
+                        !workspaceName && !workspaceDescription
+                            ? `Workspace's name and description`
+                            : !workspaceDescription
+                              ? `Workspace's description`
+                              : `Workspace's name`
+                    } can not be empty!`,
+                    type: "error",
+                });
+            }
+            if (workspaceName && workspaceDescription) {
+                const res = await workspaceApi.createWorkspace({
+                    name: workspaceName,
+                    description: workspaceDescription,
+                });
+                if (res) {
+                    onClear();
+                    notify({
+                        title: "Success!",
+                        message:
+                            "You have successfully created a new workspace!",
+                        type: "success",
+                    });
+                    onCreateWorkspace(res);
+                }
+            }
+        } catch (err) {
+            console.error(err);
         }
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    };
 
-  const renderNewWorkspaceModal = () => {
+    const renderNewWorkspaceModal = () => {
+        return (
+            <Modal
+                opened={open}
+                onClose={() => setOpen(false)}
+                title="Create new Workspace"
+                centered
+                withinPortal
+                overlayProps={{
+                    blur: 3,
+                    opacity: 0.55,
+                }}
+                styles={{
+                    inner: {
+                        alignItems: "center",
+                        justifyContent: "center",
+                    },
+                    content: {
+                        margin: 0,
+                    },
+                }}
+            >
+                <Group>
+                    <Input.Wrapper label="Name" className={classes.input}>
+                        <TextInput
+                            placeholder="Name"
+                            value={workspaceName}
+                            onChange={(e) => setWorkspaceName(e.target.value)}
+                            error={
+                                workspaceName?.includes("/")
+                                    ? "Special characters are not allowed in workspace's name!"
+                                    : workspaceName?.includes("?")
+                                      ? "Special characters are not allowed in workspace's name!"
+                                      : ""
+                            }
+                        />
+                    </Input.Wrapper>
+                    <Input.Wrapper
+                        label="Description"
+                        className={classes.input}
+                    >
+                        <Textarea
+                            placeholder="Description"
+                            minRows={3}
+                            maxRows={10}
+                            maxLength={1000}
+                            value={workspaceDescription}
+                            onChange={(e) =>
+                                setWorkspaceDescription(e.target.value)
+                            }
+                        />
+                    </Input.Wrapper>
+                </Group>
+                <Divider my="sm" />
+                <Group position="right">
+                    <Button variant="subtle" onClick={onClear}>
+                        Cancel
+                    </Button>
+                    <Button onClick={onCreateNewWorkspace}>Create</Button>
+                </Group>
+            </Modal>
+        );
+    };
+
     return (
-      <Modal
-        opened={open}
-        onClose={() => setOpen(false)}
-        title="Create new Workspace"
-        centered
-        overlayProps={{
-          blur: 3,
-          opacity: 0.55,
-        }}
-      >
-        <Group>
-          <Input.Wrapper label="Name" className={classes.input}>
-            <TextInput
-              placeholder="Name"
-              value={workspaceName}
-              onChange={(e) => setWorkspaceName(e.target.value)}
-              error={
-                workspaceName?.includes("/")
-                  ? "Special characters are not allowed in workspace's name!"
-                  : workspaceName?.includes("?")
-                  ? "Special characters are not allowed in workspace's name!"
-                  : ""
-              }
-            />
-          </Input.Wrapper>
-          <Input.Wrapper label="Description" className={classes.input}>
-            <Textarea
-              placeholder="Description"
-              minRows={3}
-              maxRows={10}
-              maxLength={1000}
-              value={workspaceDescription}
-              onChange={(e) => setWorkspaceDescription(e.target.value)}
-            />
-          </Input.Wrapper>
-        </Group>
-        <Divider my="sm" />
-        <Group position="right">
-          <Button variant="subtle" onClick={onClear}>
-            Cancel
-          </Button>
-          <Button onClick={onCreateNewWorkspace}>Create</Button>
-        </Group>
-      </Modal>
+        <>
+            <Button
+                leftIcon={<IconPlus className={classes.buttonIcon} />}
+                onClick={() => setOpen(true)}
+            >
+                New workspace
+            </Button>
+            {renderNewWorkspaceModal()}
+        </>
     );
-  };
-
-  return (
-    <>
-      <Button
-        leftIcon={<IconPlus className={classes.buttonIcon} />}
-        onClick={() => setOpen(true)}
-      >
-        New workspace
-      </Button>
-      {renderNewWorkspaceModal()}
-    </>
-  );
 };
 
 export default CreateWorkspaceButton;
